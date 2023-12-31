d5ef2d45e1ebbe6ac4d78cb93d2116b37d009505
Lewis Sneddon <lewis@lewiscodes.io>
2023-12-31 12:11:05 +0000

    bottom coordinates

--- src/main.rs
+++ src/main.rs
@@ -19,50 +19,91 @@ fn main() {
     // Coord end;
     // sunkCount = 0;
 
-    let mut board = Board::new();
-    board.cells[3] = MISS;
-    board.cells[99] = MISS;
-    println!("{}", board);
+    Game::new().show_board();
 }
 const FOG_OF_WAR: char = '~';
 const MISS: char = 'M';
 const HIT: char = 'X';
 const SHIP: char = 'O';
 
+struct Game {
+    board: Board,
+}
+
+impl Game {
+    fn new() -> Self {
+        Game {
+            board: Board::default(),
+        }
+    }
+
+    fn show_board(self) {
+        println!("{}", self.board);
+    }
+}
+
 struct Board {
     cells: Vec<char>,
     fogged_cells: Vec<char>,
-    width: usize,
-    height: usize,
+    width: u32,
+    height: u32,
 }
 
 impl Board {
-    fn new() -> Self {
+    fn new(width: u32, height: u32) -> Self {
+        let size = width * height;
         Board {
-            cells: vec![MISS; 100],
-            fogged_cells: vec![FOG_OF_WAR; 100],
-            width: 10,
-            height: 10,
+            cells: vec![MISS; size as usize],
+            fogged_cells: vec![FOG_OF_WAR; size as usize],
+            width,
+            height,
         }
+    }
+    fn default() -> Self {
+        Board::new(10, 10)
     }
 }
 
 impl Display for Board {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         writeln!(f)?;
-        for i in 0..self.height {
-            write!(f, "\t|")?;
-            for j in 0..self.width {
-                write!(f, "{}", &self.fogged_cells[(i * 10) + j])?;
-            }
-            write!(f, "|")?;
 
-            write!(f, "\t|")?;
-            for j in 0..self.width {
-                write!(f, "{}", &self.cells[(i * 10) + j])?;
+        writeln!(f)?;
+
+        for i in 0..self.height as usize {
+            write!(f, "\t")?;
+            for j in 0..self.width as usize {
+                write!(f, "{} ", &self.fogged_cells[(i * 10) + j])?;
             }
-            writeln!(f, "|")?;
+
+            write!(f, "\t")?;
+            for j in 0..self.width as usize {
+                write!(f, "{} ", &self.cells[(i * 10) + j])?;
+            }
+            writeln!(f)?;
         }
+
+        write!(f, "\t")?;
+        for _ in 0..self.width {
+            write!(f, "- ")?;
+        }
+        write!(f, "\t")?;
+        for _ in 0..self.width {
+            write!(f, "- ")?;
+        }
+        writeln!(f)?;
+
+        write!(f, "\t")?;
+        for i in 0..self.width {
+            let c = char::from_u32(i + 65).unwrap();
+            write!(f, "{} ", c)?;
+        }
+        write!(f, "\t")?;
+        for i in 0..self.width {
+            let c = char::from_u32(i + 65).unwrap();
+            write!(f, "{} ", c)?;
+        }
+
         writeln!(f)?;
         Ok(())
     }
